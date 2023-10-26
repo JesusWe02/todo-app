2d4ec8698e9efd0d4df2dd5d6ba62b29332bf81e
JesusWe02 <jesuswe02@gmail.com>
2023-10-25 23:59:23 -0500

    Implement Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-function App() {
-  return (
-    <>
-      <h1>Todo App</h1>
-    </>
-  );
-}
+import { useState } from 'react';
+import { Todos } from './Components/Todo';
+
+const mockTodos = [
+  {
+    id: '1',
+    title: 'Todo1',
+    completed: false,
+  },
+  {
+    id: '2',
+    title: 'Todo2',
+    completed: false,
+  },
+  {
+    id: '2',
+    title: 'Todo2',
+    completed: false,
+  },
+];
+
+const App = (): JSX.Element => {
+  const [todos, setTodos] = useState(mockTodos);
+  return <Todos todos={todos} />;
+};
 
 export default App;
